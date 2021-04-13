c1ec89b26c2acca0c487ca8d2f18e67cbfee3a46
zhiouYou <zhouy@vector-china.cn>
2021-04-13 22:42:52 +0800

    Vue3-P45

--- Vue3/my-project/src/App.vue
+++ Vue3/my-project/src/App.vue
@@ -1,33 +1,25 @@
 <template>
-<!-- Vue2中的html模板中，必须要一对根标签；Vue3不需要 -->
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>哈哈哈哈</div>
+  <!-- <h1>{{ num }}</h1> -->
 </template>
 
 <script lang="ts">
 // 这里可以使用ts代码
 // defineComponent函数，目的是定义一个组件，可以传入一个配置对象
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 // 暴露出去一个定义好的组件
 export default defineComponent({
   // 组件名
-  name: 'App',
-  // 注册组件
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  // 测试代码
+  // setup() {
+  //   const num = 10
+  //   return {
+  //     num,
+  //   };
+  // },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
