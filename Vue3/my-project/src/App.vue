996e984ce87dcfee88ab864786334042d64d168c
zhiouYou <zhouy@vector-china.cn>
2021-04-13 22:58:12 +0800

    setup和sef

--- Vue3/my-project/src/App.vue
+++ Vue3/my-project/src/App.vue
@@ -1,24 +1,12 @@
 <template>
-  <div>哈哈哈哈</div>
-  <!-- <h1>{{ num }}</h1> -->
+  <div></div>
 </template>
 
 <script lang="ts">
-// 这里可以使用ts代码
-// defineComponent函数，目的是定义一个组件，可以传入一个配置对象
-import { defineComponent } from "vue";
-
-// 暴露出去一个定义好的组件
+import { defineComponent, ref } from "vue";
 export default defineComponent({
   // 组件名
   name: "App",
-  // 测试代码
-  // setup() {
-  //   const num = 10
-  //   return {
-  //     num,
-  //   };
-  // },
 });
 </script>
 
