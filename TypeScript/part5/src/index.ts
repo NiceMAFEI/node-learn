a827d4319cab3256b9892652c55d0f3497615ffd
god-year <1784064251@qq.com>
2021-04-15 14:35:35 +0800

    TypeScript
    P23

--- TypeScript/part5/src/index.ts
+++ TypeScript/part5/src/index.ts
@@ -7,4 +7,24 @@ class Food {
     // 获取页面中的food元素，并赋值给element
     this.element = document.getElementById("food")!;
   }
+
+  // 获取食物x轴坐标的方法
+  get X() {
+    return this.element.offsetLeft;
+  };
+  get Y() {
+    return this.element.offsetTop;
+  };
+
+  // 修改食物位置 的方法
+  change() {
+    // 生成随机位置
+    let top =  Math.round(Math.random() * 29) * 10
+    let left =  Math.round(Math.random() * 29) * 10
+    this.element.style.left = left + 'px'
+    this.element.style.top = top + 'px'
+  }
 }
+
+
+
