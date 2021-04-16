110434d76c70a238fd8dd020748e3613055b674b
god-year <1784064251@qq.com>
2021-04-16 11:46:37 +0800

    Vue3
    P54计算属性和监视

---
+++ Vue3/my-project/06setup细节问题/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2>App父级组件</h2>
+  <h2>{{ msg }}</h2>
+  <button @click="msg += '==='">更新</button>
+  <h2>{{ count }}</h2>
+  <Child :msg="msg"/>
+</template>
+
+<script lang="ts">
+import Child from "./components/Child.vue";
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  // 组件名
+  name: "App",
+  components: {
+    Child,
+  },
+  setup() {
+    const showMsg2 = () => {
+      console.log('App中setup的showMsg2方法');
+      
+    }
+    const msg = ref('你好啊！')
+    return {
+      msg,
+      showMsg2
+    }
+  },
+  data() {
+    return {
+      count: 10
+    }
+  },
+  mounted() {
+    console.log(this);
+  },
+  methods: {
+    showMsg1() {
+      console.log('App中methods的showMsg1方法');
+    }
+  }
+});
+</script>
+
+<style></style>
---
+++ Vue3/my-project/07props和context/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>App父级组件</h2>
+  <h2>{{ msg }}</h2>
+  <button @click="msg += '==='">更新</button>
+  <h2>{{ count }}</h2>
+  <Child :msg="msg" msg2="真相" @xxx="xxx"/>
+</template>
+
+<script lang="ts">
+import Child from "./components/Child.vue";
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  // 组件名
+  name: "App",
+  components: {
+    Child,
+  },
+  setup() {
+    const showMsg2 = () => {
+      console.log('App中setup的showMsg2方法');
+      
+    }
+    const msg = ref('你好啊！')
+
+    function xxx(txt: string) {
+      msg.value += txt
+    }
+    return {
+      msg,
+      showMsg2,
+      xxx
+    }
+  },
+});
+</script>
+
+<style></style>
