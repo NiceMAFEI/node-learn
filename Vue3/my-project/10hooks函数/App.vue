85d93edf208223c63080b5f6d457b04482a26c2d
god-year <1784064251@qq.com>
2021-04-16 15:41:28 +0800

    Vue3
    P58-hooks

---
+++ Vue3/my-project/10hooks函数/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>自定义hook函数操作</h2>
+  <h2>x:{{ x }}, y: {{ y }}</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import userMousePostion from './hooks/useMousePosition'
+export default defineComponent({
+  // 组件名
+  name: "App",
+  // 发送用户点击为值的横纵坐标
+  setup() {
+    const { x, y } = userMousePostion()
+    return {
+      x,
+      y
+    };
+  },
+});
+</script>
+
+<style></style>
--- Vue3/my-project/src/App.vue
+++ Vue3/my-project/src/App.vue
@@ -1,23 +1,58 @@
 <template>
-  <h2>App父级组件</h2>
-  <button @click="isShow = !isShow">切换显示</button>
-  <hr>
-  <Child v-if="isShow"/>
+  <h2>自定义hook函数操作</h2>
+  <h2>x:{{ x }}, y: {{ y }}</h2>
+  <h3 v-if="loading">正在加载中</h3>
+  <h3 v-else-if="errorMsg">错误信息： {{ errorMsg }}</h3>
+  <ul v-else>
+    <li>id: {{ data.id }}</li>
+    <li>address: {{ data.address }}</li>
+    <li>distance: {{ data.distance }}</li>
+  </ul>
+  <hr />
+  <ul v-for="(item, index) in data" :key="index">
+    <li>id: {{ item.id }}</li>
+    <li>title: {{ item.title }}</li>
+    <li>price: {{ item.price }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
-import Child from './components/Child.vue'
-import { defineComponent, ref } from "vue";
+import { defineComponent, watch } from "vue";
+import userMousePostion from "./hooks/useMousePosition";
+import useRequest from "./hooks/useRequest";
+interface IAddressData {
+  id: number;
+  address: string;
+  distance: string;
+}
+interface IProductsData {
+  id: string;
+  title: string;
+  price: number;
+}
 export default defineComponent({
   // 组件名
   name: "App",
-  components: {
-    Child
-  },
+  // 发送用户点击为值的横纵坐标
   setup() {
-    const isShow = ref(true)
+    const { x, y } = userMousePostion();
+    // 发送请求
+    // const { loading, data, errorMsg } = useRequest<IAddressData>("/data/address.json");
+    const { loading, data, errorMsg } = useRequest<IProductsData[]>(
+      "/data/products.json"
+    );
+    // 监听
+    watch(data, () => {
+      if (data.value) {
+        console.log(data.value.length);
+      }
+    });
     return {
-      isShow
+      x,
+      y,
+      loading,
+      data,
+      errorMsg,
     };
   },
 });
