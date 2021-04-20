e4328a8790d788125588fcee45b1ac206f516e24
god-year <1784064251@qq.com>
2021-04-20 17:22:45 +0800

    Vue3-P75
    todoList案例项目搭建

--- Vue3/my-project/src/App.vue
+++ Vue3/my-project/src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <h2></h2>
+  <div class="todo-container">
+    <div class="todo-wrap">
+        <Header />
+        <List />
+        <Footer />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import Header from "./components/Header/index.vue";
+import List from "./components/List/index.vue";
+import Footer from "./components/Footer/index.vue";
 export default defineComponent({
   name: "App",
+  components: {
+      Header,
+      List,
+      Footer
+  },
   setup() {
     return {};
   },
 });
 </script>
 
-<style></style>
+<style scoped>
+.todo-container {
+    width: 600px;
+    margin: 0 auto;
+}
+.todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+}
+</style>
---
+++ Vue3/my-project/src/components/List/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="todo-main">
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+    <Item></Item>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Item from '../Item/index.vue'
+export default defineComponent({
+  name: "List",
+  components: {
+    Item
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
