258181f8863183eac1734baccdab380bb4a5c90b
godYear <9096862+godyear@user.noreply.gitee.com>
2021-07-21 15:21:19 +0800

    2021-07-21-React-P52-Context

--- react/demo-ts/src/App.tsx
+++ react/demo-ts/src/App.tsx
@@ -1,11 +1,14 @@
+import React from "react";
+import Test from "./view/test";
+import "./App.css";
 
-import Test from './test'
-import './App.css';
-
+const MyContext = React.createContext('')
 function App() {
   return (
-    <Test name='ZhouYou' age={18}></Test>
+    <MyContext.Provider value='1998-12-17'>
+      <Test name="ZhouYou" age={18}></Test>
+    </MyContext.Provider>
   );
 }
 
-export default App;
+export  {App, MyContext};
